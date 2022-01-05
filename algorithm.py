8c967283f09b2a0055166dbb09b75d9d41c6cf23
Valentin Lageard <valentin.lageard.pro@gmail.com>
2022-01-06 00:13:39 +0100

    adds faster rendering and strengths are not passed as parameters

--- algorithm.py
+++ algorithm.py
@@ -8,14 +8,14 @@ def generate_circle(radius=1, n_points=100):
     return circle
 
 
-def attract_to_connected(path, strength=0.1):
+def attract_to_connected(path):
     previous_points = np.roll(path, 1, axis=0)
     next_points = np.roll(path, -1, axis=0)
-    attract_forces = (previous_points - path) * strength + (next_points - path) * strength
+    attract_forces = (previous_points - path) + (next_points - path)
     return attract_forces
 
 
-def repulse_from_neighbours(path, strength=0.1, radius=10.0):
+def repulse_from_neighbours(path, radius=10.0):
     kdtree = KDTree(path, balanced_tree=False)
     neighbours_idxs = kdtree.query_ball_tree(kdtree, radius)
     neighbours_idxs = np.array(list(itertools.zip_longest(*neighbours_idxs, fillvalue=-1))).T
@@ -24,18 +24,18 @@ def repulse_from_neighbours(path, strength=0.1, radius=10.0):
     broadcasted_path = np.broadcast_to(np.expand_dims(path, axis=1), all_neigbours.shape)
     all_neigbours_zero_is_self = np.where(all_neigbours != [0, 0], all_neigbours, broadcasted_path)
     vectors = broadcasted_path - all_neigbours_zero_is_self
-    repulse_forces = np.sum(vectors, axis=1) * strength
+    repulse_forces = np.sum(vectors, axis=1)
     return repulse_forces
 
 
-def align_to_connected(path, strength=0.1):
+def align_to_connected(path):
     midpoints = (np.roll(path, 1, axis=0) + np.roll(path, -1, axis=0)) / 2.0
-    align_forces = (midpoints - path) * strength
+    align_forces = (midpoints - path)
     return align_forces
 
 
-def brownian_perturbate(path, strength=1.0):
-    return np.random.randn(*path.shape) * strength
+def brownian_perturbate(path):
+    return np.random.randn(*path.shape)
 
 
 def split_merge(path, split_distance=10.0, merge_distance=1.0):
@@ -55,6 +55,7 @@ def split_merge(path, split_distance=10.0, merge_distance=1.0):
             new_path[i + counter] = new_point
     return new_path
 
+
 def random_grow(path, new_points_per_frame=1):
     insertion_indexes = np.random.randint(0, path.shape[0], new_points_per_frame)
     points = path[insertion_indexes]
@@ -62,12 +63,13 @@ def random_grow(path, new_points_per_frame=1):
     midpoints = (points + next_points) / 2.0
     return np.insert(path, insertion_indexes, midpoints, axis=0)
 
+
 def differential_growth(path, attraction_strength=0.1, repulsion_strength=0.1, repulsion_radius=10.0,
-         brownian_strength=1.0, align_strength=0.1, split_distance=10.0, merge_distance=1.0):
-    attract_forces = attract_to_connected(path, attraction_strength)
-    repulse_forces = repulse_from_neighbours(path, repulsion_strength, repulsion_radius)
-    brownian_forces = brownian_perturbate(path, brownian_strength)
-    align_forces = align_to_connected(path, align_strength)
+                        brownian_strength=1.0, align_strength=0.1, split_distance=10.0, merge_distance=1.0):
+    attract_forces = attract_to_connected(path) * attraction_strength
+    repulse_forces = repulse_from_neighbours(path, repulsion_radius) * repulsion_strength
+    brownian_forces = brownian_perturbate(path) * brownian_strength
+    align_forces = align_to_connected(path) * align_strength
     new_path = split_merge(path + attract_forces + repulse_forces + brownian_forces + align_forces,
                            split_distance, merge_distance)
     new_path = random_grow(new_path)
--- main.py
+++ main.py
@@ -17,11 +17,15 @@ def create_center_line(canvas, point1, point2):
 
 
 def draw_path(canvas, path):
-    for i in range(path.shape[0]):
-        create_center_line(canvas, path[i], path[(i + 1) % len(path)])
-        # canvas.create_oval(path[i][0] - 3 + WIDTH / 2, path[i][1] - 3 + HEIGHT / 2,
-        #                    path[i][0] + 3 + WIDTH / 2, path[i][1] + 3 + HEIGHT / 2,
-        #                    fill='red')
+    centered_path_plus_one = np.vstack([path, path[0]]) + np.array([WIDTH / 2, HEIGHT / 2])
+    canvas.create_line(*(centered_path_plus_one.flatten()),
+                       fill='white', smooth=1, splinesteps=0)
+    # for i in range(path.shape[0]):
+    #     create_center_line(canvas, path[i], path[(i + 1) % len(path)])
+    #     canvas.create_oval(path[i][0] - 3 + WIDTH / 2, path[i][1] - 3 + HEIGHT / 2,
+    #                        path[i][0] + 3 + WIDTH / 2, path[i][1] + 3 + HEIGHT / 2,
+    #                        fill='red')
+
 
 # INTERACTIONS
 
@@ -48,7 +52,7 @@ def main():
     canvas.configure(bg='black')
     canvas.pack()
 
-    path = generate_circle(100, 20)
+    path = generate_circle(200, 3)
 
     while True:
         if playing:
@@ -58,9 +62,9 @@ def main():
         app.update()
 
         if playing:
-            path = differential_growth(path, attraction_strength=0.1, repulsion_strength=0.05, repulsion_radius=10.0,
-                                       brownian_strength=0.1, align_strength=0.1, split_distance=6.0, merge_distance=2.0)
-            #time.sleep(1/60)
+            path = differential_growth(path, attraction_strength=0.05, repulsion_strength=0.01, repulsion_radius=5.0,
+                                       brownian_strength=0.0, align_strength=0, split_distance=2.0, merge_distance=1.0)
+            time.sleep(1/120)
             canvas.delete("all")
             print("Nodes: ", path.shape[0])
 
