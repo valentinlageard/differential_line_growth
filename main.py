cba701bbcd6563432300565e3c75caa376518ce7
Valentin Lageard <valentin.lageard.pro@gmail.com>
2022-01-07 19:17:37 +0100

    adds tracing enabling interaction when pressing A

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyglet
 import pyglet.gl as gl
+import pyglet.window.key as key
 from algorithm import *
 
 WIDTH = 1500
@@ -12,7 +13,7 @@ class SimulationWindow(pyglet.window.Window):
 
         self.batch = pyglet.graphics.Batch()
         self.path = generate_circle(200, 100)
-        centered_path = self.path + np.array([WIDTH / 2, HEIGHT / 2])
+        centered_path = self.path + np.array([self.width / 2, self.height / 2])
         self.vertex_list = self.batch.add(self.path.shape[0], gl.GL_LINE_LOOP, None,
                                           ('v2f', list(centered_path.flatten())),
                                           ('c4f', (1, 1, 1, 0.01) * self.path.shape[0]))
@@ -24,7 +25,8 @@ class SimulationWindow(pyglet.window.Window):
         gl.glEnable(gl.GL_BLEND)
         gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
 
-        self.playing = True  # TODO: to use for play/pause interaction
+        self.playing = True
+        self.tracing = False
 
         pyglet.clock.schedule_interval(self.update, 1 / 120.0)
         self.clear()
@@ -36,24 +38,29 @@ class SimulationWindow(pyglet.window.Window):
                                             attraction_strength=0.2, repulsion_strength=0.018, repulsion_radius=30.0,
                                             brownian_strength=0.09, align_strength=0.01, split_distance=10.0,
                                             merge_distance=5.0)
-            centered_path = self.path + np.array([WIDTH / 2, HEIGHT / 2])
+            centered_path = self.path + np.array([self.width / 2, self.height / 2])
             self.vertex_list.resize(self.path.shape[0])
             self.vertex_list.vertices = list(centered_path.flatten())
-            self.vertex_list.colors = (1, 1, 1, 0.005) * self.path.shape[0]
+            color = (1, 1, 1, 0.005) if self.tracing else (1, 1, 1, 1)
+            self.vertex_list.colors = color * self.path.shape[0]
             print("Nodes: ", self.path.shape[0])
 
     def on_draw(self):
-        # self.clear()
-        # self.fps_display.draw()
         if self.playing:
+            if not self.tracing:
+                self.clear()
+                self.fps_display.draw()
             self.batch.draw()
 
     def on_close(self):
         self.close()
 
     def on_key_press(self, symbol, modifiers):
-        if symbol == pyglet.window.key.SPACE:
+        if symbol == key.SPACE:
             self.playing = not self.playing
+        if symbol == key.A:
+            self.tracing = not self.tracing
+
 
 
 def main():
