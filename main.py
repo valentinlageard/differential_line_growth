5e483c1a27a1ae5e0ae3e1ee0895a828f212392e
Valentin Lageard <valentin.lageard.pro@gmail.com>
2022-01-08 20:39:32 +0100

    adds interactions for growth and repulsion

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import pyglet.window.key as key
 from algorithm import *
 
 
-WIDTH = 1500
-HEIGHT = 1040
-
-
 class SimulationWindow(pyglet.window.Window):
     def __init__(self, dlgconf: DLGConf, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,7 +12,7 @@ class SimulationWindow(pyglet.window.Window):
 
         self.batch = pyglet.graphics.Batch()
 
-        self.path = generate_circle(200, 100)
+        self.path = generate_circle(100, 100)
         centered_path = self.path + np.array([self.width / 2, self.height / 2])
         self.vertex_list = self.batch.add(self.path.shape[0], gl.GL_LINE_LOOP, None,
                                           ('v2f', list(centered_path.flatten())),
@@ -24,7 +20,7 @@ class SimulationWindow(pyglet.window.Window):
 
         self.fps_display = pyglet.window.FPSDisplay(window=self)
         self.fps_display.label.color = (255, 255, 255, 255)
-        self.debug_label = pyglet.text.Label(x=0, y=self.height - 15, width=400, height=400, multiline=True)
+        self.debug_label = pyglet.text.Label(x=5, y=self.height - 15, width=400, height=400, multiline=True)
 
         gl.glLineWidth(1)
         gl.glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -65,13 +61,17 @@ class SimulationWindow(pyglet.window.Window):
             self.clear()
             self.path = generate_circle(200, 100)
         if symbol == key.UP:
-            self.dlgconf.scale += 0.1
-            if self.dlgconf.scale > 2.0:
-                self.dlgconf.scale = 2.0
+            self.dlgconf.scale = min(self.dlgconf.scale + 0.05, 1.5)
         if symbol == key.DOWN:
-            self.dlgconf.scale -= 0.1
-            if self.dlgconf.scale < 0.1:
-                self.dlgconf.scale = 0.1
+            self.dlgconf.scale = max(self.dlgconf.scale - 0.05, 0.05)
+        if symbol == key.Q:
+            self.dlgconf.growth = min(self.dlgconf.growth + 0.01, 0.5)
+        if symbol == key.W:
+            self.dlgconf.growth = max(self.dlgconf.growth - 0.01, 0.01)
+        if symbol == key.S:
+            self.dlgconf.repulsion = min(self.dlgconf.repulsion + 0.1, 10.0)
+        if symbol == key.X:
+            self.dlgconf.repulsion = max(self.dlgconf.repulsion - 0.1, 0.1)
 
     def _update_vertex_list(self):
         centered_path = self.path + np.array([self.width / 2, self.height / 2])
@@ -95,7 +95,7 @@ def main():
                               max_distance=15.0,
                               scale=1.0)
     window_conf = pyglet.gl.Config(sample_buffers=1, sample=4)
-    window = SimulationWindow(dlgconf=simulation_conf, width=WIDTH, height=HEIGHT, config=window_conf)
+    window = SimulationWindow(dlgconf=simulation_conf, fullscreen=True, config=window_conf)
     pyglet.app.run()
 
 
